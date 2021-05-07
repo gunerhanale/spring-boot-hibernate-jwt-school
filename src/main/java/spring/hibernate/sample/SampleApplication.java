3deceaadc5728b2a96fe44ac4e5c3279f0cbb3a9
gunerhanale <gunerhanale@gmail.com>
2021-05-07 21:20:07 +0200

    created entities for school sample

--- src/main/java/spring/hibernate/sample/SampleApplication.java
+++ src/main/java/spring/hibernate/sample/SampleApplication.java
@@ -1,13 +1,22 @@
 package spring.hibernate.sample;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import spring.hibernate.dto.Student;
 
 @SpringBootApplication
-public class SampleApplication {
+public class SampleApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Student userDetails = new Student();
+		userDetails.setUserId(1);
+		userDetails.setUserName("First User");
 	}
 
 }
