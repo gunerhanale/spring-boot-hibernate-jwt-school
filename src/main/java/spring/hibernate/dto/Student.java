95bce7f67c45be14f6e0a328629ce118af632006
gunerhanale <gunerhanale@gmail.com>
2021-05-09 17:19:22 +0200

    added @JsonIgnore annotation to fix recursive issue at ManyToMany relationship

--- src/main/java/spring/hibernate/dto/Student.java
+++ src/main/java/spring/hibernate/dto/Student.java
@@ -1,5 +1,7 @@
 package spring.hibernate.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
@@ -11,6 +13,7 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonIgnore
     @ManyToMany(mappedBy = "enrolledStudents")
     private Set<Subject> subjects = new HashSet<>();
 
